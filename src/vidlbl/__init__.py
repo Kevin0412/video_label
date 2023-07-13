b0017d72fc927a295e1405593505609380451bd4
Kevin0412 <45508696+Kevin0412@users.noreply.github.com>
2023-07-13 15:01:28 +0800

    2倍速，0.5倍速

--- src/vidlbl/__init__.py
+++ src/vidlbl/__init__.py
@@ -24,31 +24,49 @@ class video:
 
     def show(self,speed=1):
         cap = cv2.VideoCapture(self.path)
-        FPS=cap.get(5)*speed
         n=0
+        FPS=cap.get(5)
         start_time=time.time()
         prev_time=time.time()-start_time
         while cap.isOpened():
             if n!=0:
-                while curr_time>=n/FPS:
+                while curr_time>=n/FPS/speed:
                     ret, img = cap.read()
                     n+=1
             else:
-                while prev_time>=n/FPS:
+                while prev_time>=n/FPS/speed:
                     ret, img = cap.read()
                     n+=1
                 curr_time = time.time()-start_time
             if not ret:
                 break
-            if curr_time>=(n-1)/FPS and curr_time<=n/FPS:
+            if curr_time>=(n-1)/FPS/speed and curr_time<=n/FPS/speed:
                 if img.shape[0]>720 or img.shape[1]>1280:
                     img = cv2.resize(img, None, fx=0.5, fy=0.5)
+                cv2.imshow('img',img)
                 curr_time = time.time()-start_time
                 fps = 1/(curr_time-prev_time)
                 prev_time = curr_time
-                cv2.imshow('img',img)
-            key = cv2.waitKey(1)
-            print('\r'+str(curr_time*speed),end='')
-            if key==27:
-                break
+                key = cv2.waitKey(1)
+                if key==27:
+                    break
+                elif key==ord('w'):
+                    start_time+=curr_time*(1-speed/2)
+                    prev_time=time.time()-start_time
+                    speed=2
+                    print('\n2x speed')
+                    curr_time=time.time()-start_time
+                elif key==ord('s'):
+                    start_time+=curr_time*(1-speed)
+                    prev_time=time.time()-start_time
+                    speed=1
+                    print('\n1x speed')
+                    curr_time=time.time()-start_time
+                elif key==ord('x'):
+                    start_time+=curr_time*(1-speed*2)
+                    prev_time=time.time()-start_time
+                    speed=0.5
+                    print('\n0.5x speed')
+                    curr_time=time.time()-start_time
+                print('\r'+str(int(curr_time*speed*100)/100)+'\t'+str(int(fps*10)/10),end='')
         cap.release()
